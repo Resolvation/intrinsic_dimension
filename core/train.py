f0b6153a3fb81b42797d7afc1b77b4b6f378abdf
Resolvation <32676455+Resolvation@users.noreply.github.com>
2018-11-16 00:49:07 +0300

    Update train.py

--- core/train.py
+++ core/train.py
@@ -2,44 +2,56 @@ import datetime as dt
 
 import torch.nn.functional as F
 
+from core.evaluate import evaluate
 import core.utils as utils
 
 
-def stdn_loss(net, dataset_len):
+def stdn_loss(model, dataset_len):
     """
     Return loss function for variational nn with stdn prior.
     """
     def loss(outputs, labels):
-        return F.cross_entropy(outputs, labels) + net.KL()/dataset_len
+        return F.cross_entropy(outputs, labels) + model.KL()/dataset_len
     return loss
 
 
-def train(device, net, train_loader, criterion, optimizer, n_epochs=200):
+def train(device, model, train_loader, test_loader,
+          criterion, optimizer, test_criterion=None, n_epochs=200):
     """
-    Train net on device using optimizer to minimize criterion on train_loader
+    Train model on device using optimizer to minimize criterion on train_loader
     for n_epochs.
     """
     print("Training:")
-    steps, losses = [0], []
     init_lr = utils.lr(optimizer)
     start = dt.datetime.now()
     for epoch in range(n_epochs):
         epoch_start = dt.datetime.now()
+        epoch_correct, epoch_loss = 0., 0.
         optimal_lr = utils.lr_linear(epoch, n_epochs/2, n_epochs, init_lr)
         utils.adjust_learning_rate(optimizer, optimal_lr)
-        batch_loss = 0
-        for batch_idx, (images, labels) in enumerate(train_loader):
+        for images, labels in train_loader:
             images, labels = images.to(device), labels.to(device)
             optimizer.zero_grad()
-            outputs = net(images)
+            outputs = model(images)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-            batch_loss += loss.item()
-            if (batch_idx + 1) % n_epochs == 0:
-                steps.append(steps[-1]+labels.shape[0])
-                losses.append(loss.item())
-        if (epoch + 1) % 5 == 0:
-            print("Epoch: {:>03}    Loss: {:>010.4f}    Time: {:8}".format(epoch+1, batch_loss/len(train_loader),
-                                                                 str(dt.datetime.now()-epoch_start).split('.')[0]))
+            pred = outputs.max(1, keepdim=True)[1]
+            epoch_correct += pred.eq(labels.view_as(pred)).sum().item()
+            epoch_loss += loss.item()
+        fmt = ("Epoch: {:>03}    Loss: {:>010.4f}    "
+               "Accuracy: {:>.2f}    Time: {:8}")
+        print(fmt.format(epoch+1,
+                         epoch_loss / len(train_loader),
+                         100 * epoch_correct / len(train_loader.dataset),
+                         str(dt.datetime.now()-epoch_start).split('.')[0]))
+        if (epoch + 1) % 20 == 0:
+            print("After {:} epochs".format(epoch+1))
+            print("Trainset:")
+            evaluate(device, model, train_loader, criterion)
+            print("Testset:")
+            if test_criterion is None:
+                evaluate(device, model, test_loader, criterion)
+            else:
+                evaluate(device, model, test_loader, test_criterion)
     print("Time: {:8}".format(str(dt.datetime.now()-start).split('.')[0]))
