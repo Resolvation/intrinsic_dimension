ab3257d6c2bbc4f8160c81f0e7d3b711631338d1
Resolvation <32676455+Resolvation@users.noreply.github.com>
2018-11-16 00:47:02 +0300

    Update lenet-300-100-id-l.py

--- lenet-300-100-id-l.py
+++ lenet-300-100-id-l.py
@@ -14,40 +14,32 @@ def main():
     torch.manual_seed(42)
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    net = LeNet_300_100_ID_L(450, 250, 50).to(device)
-    criterion = nn.CrossEntropyLoss()
+    model = LeNet_300_100_ID_L(450, 250, 50).to(device)
     train_loader, test_loader = mnist_data()
-    optimizer = optim.Adam(net.parameters())
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters())
 
-    train(device, net, train_loader, criterion, optimizer)
+    train(device, model, train_loader, test_loader, criterion, optimizer)
 
-    print("Trainset:")
-    evaluate(device, net, train_loader, criterion)
-    print("Testset:")
-    evaluate(device, net, test_loader, criterion)
-
-    save(net, "LeNet_300_100_ID_L")
+    save(model, "LeNet_300_100_ID_L")
 
 
 class LeNet_300_100_ID_L(nn.Module):
     """
-    Neural network with two linear layers of size 300 and 100 in intristic
+    Neural modelwork with two linear layers of size 300 and 100 in intristic
     dimentions of size d1, d2, d3.
     """
     def __init__(self, d1, d2, d3):
         super().__init__()
-        self.register_parameter("theta_d1", nn.Parameter(torch.zeros(d1)))
-        self.register_parameter("theta_d2", nn.Parameter(torch.zeros(d2)))
-        self.register_parameter("theta_d3", nn.Parameter(torch.zeros(d3)))
         self.l1 = LinearOffset(d1, 28*28, 300)
         self.l2 = LinearOffset(d2, 300, 100)
         self.l3 = LinearOffset(d3, 100, 10)
 
     def forward(self, x):
         x = x.view(-1, 28*28)
-        x = F.relu(self.l1(x, self.theta_d1))
-        x = F.relu(self.l2(x, self.theta_d2))
-        return self.l3(x, self.theta_d3)
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        return self.l3(x)
 
 
 if __name__ == "__main__":
