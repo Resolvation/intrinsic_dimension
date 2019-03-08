27b8a081c456f1c2d5b20d5a370758ed90c875cb
Anuar Askergaliyev <anuar.askergali@gmail.com>
2019-03-09 00:06:51 +0300

    Added standart gaussian variational lower bound.

--- core/utils.py
+++ core/utils.py
@@ -1,6 +1,8 @@
 import os
 
 import torch
+from torch import nn
+from torch.nn import functional as F
 
 
 def adjust_learning_rate(optimizer, lr):
@@ -16,3 +18,18 @@ def save_model(model, name, path='../tars'):
     if not os.path.exists(path):
         os.mkdir(path)
     torch.save(model.state_dict(), path+'/'+name+'.tar')
+
+
+class SGVLB(nn.Module):
+    def __init__(self, network, dataset_size):
+        super().__init__()
+        self.dataset_size = dataset_size
+        self.network = network
+
+    def forward(self, input, target, kl_weight=1.):
+        kl = 0.
+        for child in self.network.children():
+            if hasattr(child, 'kl'):
+                kl += child.kl()
+        return (F.cross_entropy(input, target, size_average=True)
+                * self.dataset_size) + kl_weight * kl
